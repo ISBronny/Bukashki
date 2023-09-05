14fd469b6ef1936eb85ae57dc2a86ac91e19a697
Ivan Braiko <isbraiko@gmail.com>
2023-09-05 18:31:20 +0300

    Initial

---
+++ Bukashki.Domain/Models/Class1.cs
@@ -0,0 +1,195 @@
+﻿using System.Drawing;
+using System.Numerics;
+
+namespace Bukashki.Domain.Models;
+
+public class AntManager
+{
+	public List<Ant> Ants;
+    
+
+	public List<IDestination> Destinations = new List<IDestination>()
+	{
+		new Home()
+		{
+			Point = new(200, 250)
+		},
+		new Home()
+		{
+			Point = new(1000, 400)
+		},
+		new Home()
+		{
+			Point = new(1200, 1000),
+		},
+		new Resource()
+		{
+			Point = new(600, 200)
+		},
+		new Resource()
+		{
+			Point = new(400, 800)
+		},
+		new Resource()
+		{
+			Point = new(1200, 200),
+		},
+	};
+
+
+	public AntManager(Rectangle bounds)
+	{
+		var antMovement = new AntMovementStrategy(bounds);
+		Ants = Enumerable.Range(0, 1000).Select(i => new Ant(
+			antMovement
+		)
+		{
+			Point = new(bounds.Right - Random.Shared.NextSingle() * bounds.Width,
+				bounds.Bottom - Random.Shared.NextSingle() * bounds.Height),
+			Destination = Destinations[Random.Shared.Next(Destinations.Count)],
+			OnDestinate = ant =>
+			{
+				ant.Destination = Destinations[Random.Shared.Next(Destinations.Count)];
+				ant.Direction = -ant.Direction;
+			},
+			Counter = Destinations.ToDictionary(x=>x, x=>0),
+			OnCry = m =>
+			{
+				foreach (var childAnt in Ants.AsParallel()
+					         .Where(a => (a.Point - m.Sender.Point).Length() <= m.Sender.CryDistance))
+				{
+					childAnt.Listen(m);
+				}
+
+				m.Sender.ReceivedMessage = null;
+			}
+		}).ToList();
+	}
+
+	public void Step()
+	{
+		foreach (var ant in Ants)
+		{
+			ant.Step();
+		}
+	}
+}
+
+public class Ant
+{
+	public Vector2 Point;
+	public IDestination Destination { get; set; }
+	public Dictionary<IDestination, int> Counter { get; set; }
+	
+	public Vector2 Direction = Vector2.Normalize(new Vector2(Random.Shared.NextSingle() - 0.5f, Random.Shared.NextSingle() - 0.5f));
+
+	public int CryDistance { get; set; } = 100;
+
+	public Action<Ant> OnDestinate { get; init; }
+	public Action<(Ant Sender, IDestination Destination)> OnCry { get; init; }
+	
+	
+	//For graphics
+	public (Ant Sender, IDestination Destination)? ReceivedMessage = null;
+
+	private Action? OnNextMove;
+
+	private IMovementStrategy _movementStrategy;
+
+	public Ant(IMovementStrategy movementStrategy)
+	{
+		_movementStrategy = movementStrategy;
+	}
+
+
+	public void Step()
+	{
+		if (OnNextMove is not null)
+		{
+			OnNextMove();
+			OnNextMove = null;
+		}
+		if(ReceivedMessage.HasValue)
+		{
+			var message = (this, ReceivedMessage.Value.Destination);
+			OnNextMove = () => OnCry(message);
+		}
+
+		foreach (var dest in Counter.Keys)
+		{
+			if ((Point - dest.Point).Length() < 20)
+			{
+				Counter[dest] = 0;
+				OnCry((this, Destination));
+				if(Destination == dest)
+					OnDestinate.Invoke(this);
+			}
+		}
+		
+        
+		foreach (var key in Counter.Keys) Counter[key]++;
+
+		_movementStrategy.Move(ref Point, ref Direction);
+	}
+
+	public void Listen((Ant Sender, IDestination Destination) message)
+	{
+		var (sender, destination) = message;
+		if (Counter[destination] > sender.Counter[destination] + sender.CryDistance)
+		{
+			Counter[destination] = sender.Counter[destination] + sender.CryDistance;
+			if(destination == Destination)
+				Direction =  Vector2.Normalize(sender.Point - Point);
+            ReceivedMessage = message;
+		}
+
+	}
+}
+
+public interface IMovementStrategy
+{
+	public void Move(ref Vector2 point, ref Vector2 direction);
+}
+
+public class AntMovementStrategy : IMovementStrategy
+{
+	private Rectangle Bounds { get; }
+	private float Speed { get; set; } = 1.0f;
+
+	public AntMovementStrategy(Rectangle bounds)
+	{
+		Bounds = bounds;
+	}
+	public void Move(ref Vector2 point, ref Vector2 direction)
+	{
+		if (point.X + Speed * direction.X > Bounds.Right)
+			direction.X = -Math.Abs(direction.X);
+		if (point.X + Speed * direction.X < Bounds.Left)
+			direction.X = Math.Abs(direction.X);
+		
+		if (point.Y + Speed * direction.Y < Bounds.Top)
+			direction.Y = Math.Abs(direction.Y);
+		if (point.Y + Speed * direction.Y > Bounds.Bottom)
+			direction.Y = -Math.Abs(direction.Y);
+
+		point += Speed * direction;
+	}
+}
+
+public interface IDestination
+{
+	public string Id { get; }
+	public Vector2 Point { get; }
+}
+
+public class Home : IDestination
+{
+	public string Id { get; init; }
+	public Vector2 Point { get; init; }
+}
+
+public class Resource : IDestination
+{
+	public string Id { get; init; }
+	public Vector2 Point { get; init; }
+}
---
+++ Bukashki/MainWindow.xaml.cs
@@ -0,0 +1,243 @@
+﻿using System;
+using System.Drawing;
+using System.Linq;
+using System.Windows;
+using System.Windows.Media;
+using System.Windows.Threading;
+using Bukashki.Domain.Models;
+using Point = System.Windows.Point;
+
+namespace Bukashki
+{
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+		private AntManager _antManager;
+		public object _mutext = new object();
+
+		public MainWindow()
+		{
+			InitializeComponent();
+
+			_antManager = new AntManager(new Rectangle(0, 0, (int)Image.Width, (int)Image.Height))
+			{
+			};
+
+			DispatcherTimer t = new DispatcherTimer();
+			t.Tick += t_Tick;
+			t.Interval = new TimeSpan(0, 0, 0, 0, 20);
+			t.Start();
+
+		}
+
+		GeometryGroup antsEllipses;
+
+		public void t_Tick(object? sender, EventArgs e)
+		{
+			_antManager.Step();
+
+
+			antsEllipses = new GeometryGroup();
+
+			if (!antsEllipses.Children.Any())
+				foreach (var ant in _antManager.Ants)
+					antsEllipses.Children.Add(new EllipseGeometry(new Point(ant.Point.X, ant.Point.Y), 1, 1));
+			else
+				for (int i = 0; i <= _antManager.Ants.Count; i++)
+					((EllipseGeometry)antsEllipses.Children[i]).Center =
+						new Point(_antManager.Ants[i].Point.X, _antManager.Ants[i].Point.Y);
+
+
+			GeometryDrawing ants = new GeometryDrawing();
+			ants.Geometry = antsEllipses;
+			ants.Brush = Brushes.Azure;
+
+			GeometryGroup homeEllipses = new GeometryGroup();
+			foreach (var destination in _antManager.Destinations.OfType<Home>())
+			{
+				homeEllipses.Children.Add(
+					new EllipseGeometry(new Point(destination.Point.X, destination.Point.Y), 20, 20));
+			}
+
+
+			GeometryGroup resEllipses = new GeometryGroup();
+			foreach (var destination in _antManager.Destinations.OfType<Resource>())
+			{
+				resEllipses.Children.Add(
+					new EllipseGeometry(new Point(destination.Point.X, destination.Point.Y), 20, 20));
+			}
+
+
+			GeometryDrawing home = new GeometryDrawing();
+			home.Geometry = homeEllipses;
+			home.Brush = Brushes.Yellow;
+
+			GeometryDrawing res = new GeometryDrawing();
+			res.Geometry = resEllipses;
+			res.Brush = Brushes.DodgerBlue;
+
+
+
+			var linesHome = new GeometryGroup();
+			var linesRes = new GeometryGroup();
+
+
+			foreach (var ant in _antManager.Ants)
+			{
+				var m = ant.ReceivedMessage;
+				if (m is null)
+					continue;
+				var line = new LineGeometry(new Point(ant.Point.X, ant.Point.Y),
+					new Point(m.Value.Sender.Point.X, m.Value.Sender.Point.Y));
+				if (m.Value.Destination is Home)
+					linesHome.Children.Add(line);
+				else
+					linesRes.Children.Add(line);
+
+			}
+
+
+			/*GeometryDrawing homelinksSolid = new GeometryDrawing();
+			homelinksSolid.Geometry = linesHome;
+			homelinksSolid.Brush = Brushes.Yellow;
+
+			GeometryDrawing reslinksSolid = new GeometryDrawing();
+			reslinksSolid.Geometry = linesRes;
+			reslinksSolid.Brush = Brushes.DodgerBlue;*/
+
+
+
+			var collection = new DrawingCollection()
+			{
+				ants,
+
+				//homelinksSolid,
+				//reslinksSolid,
+
+				new GeometryDrawing()
+				{
+					Geometry = new RectangleGeometry(new Rect(-15, -15, Image.Width + 15, Image.Height + 15))
+					{
+
+					},
+					Brush = Brushes.Transparent
+				}
+
+			};
+
+			foreach (var line in linesHome.Children)
+			{
+				var color = System.Windows.Media.Color.FromRgb(245, 218, 66);
+
+				GeometryDrawing glowHome = new GeometryDrawing();
+				glowHome.Geometry = line;
+				glowHome.Pen = new Pen(new SolidColorBrush(color)
+				{
+					Opacity = 1,
+				}, 1);
+				
+				
+
+				GeometryDrawing glowHome2 = new GeometryDrawing();
+				glowHome2.Geometry = line;
+				glowHome2.Pen = new Pen(new SolidColorBrush(color)
+				{
+					Opacity = 0.2
+				}, 10)
+				{
+					EndLineCap = PenLineCap.Round,
+					StartLineCap = PenLineCap.Round
+				};
+				
+				
+				GeometryDrawing glowHome3 = new GeometryDrawing();
+				glowHome3.Geometry = line;
+				glowHome3.Pen = new Pen(new SolidColorBrush(color)
+				{
+					Opacity = 0.1
+				}, 20){
+					EndLineCap = PenLineCap.Round,
+					StartLineCap = PenLineCap.Round
+				};
+
+				collection.Add(glowHome3);
+				collection.Add(glowHome2);
+				collection.Add(glowHome);
+
+			}
+
+			foreach (var line in linesRes.Children)
+			{
+				var color = System.Windows.Media.Color.FromRgb(0, 96, 191);
+
+				GeometryDrawing glowRes = new GeometryDrawing();
+				glowRes.Geometry = line;
+				glowRes.Pen = new Pen(new SolidColorBrush(color)
+				{
+					Opacity = 1
+				}, 1);
+				
+				GeometryDrawing glowRes2 = new GeometryDrawing();
+				glowRes2.Geometry = line;
+				glowRes2.Pen = new Pen(new SolidColorBrush(color)
+				{
+					Opacity = 0.2
+				}, 10){
+					EndLineCap = PenLineCap.Round,
+					StartLineCap = PenLineCap.Round
+				};
+				
+				
+				GeometryDrawing glowRes3 = new GeometryDrawing();
+				glowRes3.Geometry = line;
+				glowRes3.Pen = new Pen(new SolidColorBrush(color)
+				{
+					Opacity = 0.1
+				}, 20){
+					EndLineCap = PenLineCap.Round,
+					StartLineCap = PenLineCap.Round
+				};
+				
+				
+				collection.Add(glowRes3);
+				collection.Add(glowRes2);
+				collection.Add(glowRes);
+
+			}
+
+			collection.Add(res);
+			collection.Add(home);
+
+
+
+			DrawingImage geometryImage = new DrawingImage()
+			{
+				Drawing = new DrawingGroup()
+				{
+					Children = collection,
+				}
+			};
+
+
+			// Freeze the DrawingImage for performance benefits.
+			geometryImage.Freeze();
+
+
+			lock (_mutext)
+			{
+				Image.Source = geometryImage;
+				Image.Stretch = Stretch.None;
+				Image.HorizontalAlignment = HorizontalAlignment.Center;
+				Image.VerticalAlignment = VerticalAlignment.Center;
+			}
+
+
+		}
+
+		private void Image_OnSizeChanged(object sender, SizeChangedEventArgs e)
+		{
+		}
+	}
+}
